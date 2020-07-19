5b1ebcece4601fbd949406fa739e140867233b1f
Christoph Krieg <christoph.krieg@365businessdev.com>
2020-07-19 19:08:08 +0200

    Disable SignatureHelpProvider (prototype)

--- src/DoHover.ts
+++ src/DoHover.ts
@@ -9,9 +9,9 @@ export class DoHover {
             languages.registerHoverProvider('al', new ALHoverProvider());            
         }                
         // check configuration for enabled Parameter presentation
-        if (workspace.getConfiguration("bdev-al-xml-doc").enableSignatureHover) {
-            languages.registerSignatureHelpProvider('al', new ALSignatureHelpProvider(), '(', ',');
-        }
+        // if (workspace.getConfiguration("bdev-al-xml-doc").enableSignatureHover) {
+        //     languages.registerSignatureHelpProvider('al', new ALSignatureHelpProvider(), '(', ',');
+        // }
     }
 
     public dispose() {
