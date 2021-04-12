1e38cf0096b451d3f58699282696d4f46fbd72aa
Christoph Krieg <christoph.krieg@365businessdev.com>
2021-04-12 21:14:14 +0200

    CSS: table width 100%

--- src/util/markdown/MarkdownWriter.ts
+++ src/util/markdown/MarkdownWriter.ts
@@ -87,6 +87,6 @@ export class MarkdownWriter {
      * @returns Default stylesheet definition .
      */
     public static GetDefaultStylesheet(): string {
-        return `.page-header{margin:0 auto;font-family: Segoe UI Semibold;font-size: 10px;}.page-footer{margin-left: 50px;font-family:Segoe UI;font-size:9px}h1{font-size:28px}h2{font-size:26px}h3{font-size:23px}h4{font-size:22px}h5{font-size:20px}`;
+        return `.page-header{margin:0 auto;font-family: Segoe UI Semibold;font-size: 10px;}.page-footer{margin-left: 50px;font-family:Segoe UI;font-size:9px}h1{font-size:28px}h2{font-size:26px}h3{font-size:23px}h4{font-size:22px}h5{font-size:20px}table{width:100%}`;
     }
 }
